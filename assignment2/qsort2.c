de45d60785e15bac64e6bdd45c42cb0d27ad5851
Anders Hassis <anders.hassis@gmail.com>
2013-02-28 11:38:02 +0100

    First commit of quick sort with pthreads

---
+++ assignment2/qsort2.c
@@ -0,0 +1,162 @@
+// quickSort.c
+#include <stdio.h>
+#include <stdlib.h> 
+#include <pthread.h>
+#include <time.h>
+#include <math.h>
+
+#define NUM_THREADS 50
+#define INTERVALS 100000000
+#define DEBUG 0
+
+void qsort_serial( int l, int r);
+void *threadQuickSort(void *arg);
+int partition_serial( int l, int r);
+
+pthread_mutex_t mutexsort;
+
+struct thread_data {
+  int start_index;
+  int end_index;
+};
+
+int thread_count = 0;
+int *elements;
+
+void *qsort_parallel(void *arg) {
+  struct thread_data index_left, index_right;
+  int start_index, end_index, pivot;
+
+  void *status;
+  pthread_t thread1, thread2;
+  
+  struct thread_data *args;
+  args = (struct thread_data *) arg;
+  start_index = args->start_index;
+  end_index = args->end_index;
+
+  if (start_index < end_index) {
+    if (end_index-start_index > (0.1*INTERVALS) && thread_count < NUM_THREADS-2) {
+      pivot = partition_serial(start_index, end_index);
+
+      index_left.start_index = start_index;
+      index_left.end_index = pivot-1;
+
+      index_right.start_index = pivot+1;
+      index_right.end_index = end_index;
+
+      pthread_mutex_lock(&mutexsort);
+      thread_count += 2;
+      pthread_mutex_unlock(&mutexsort);
+
+      pthread_create(&thread1, NULL, qsort_parallel,(void *) &index_left);
+      pthread_create(&thread2, NULL, qsort_parallel,(void *) &index_right);
+
+      pthread_join(thread1, &status);
+      pthread_join(thread2, &status);
+
+    } else {
+      qsort_serial(start_index, end_index);
+    }
+  }
+
+  pthread_exit(NULL);
+}
+
+void qsort_serial( int l, int r) {
+   int pivot;
+
+   if (l < r)  {
+      pivot = partition_serial(l, r);
+
+      qsort_serial(l, pivot-1);
+      qsort_serial(pivot+1, r);
+   }
+}
+
+int partition_serial( int l, int r) {
+  int pivot, i, j, t; 
+  pivot = elements[l];
+  i = l; 
+  j = r+1;
+    
+  while (1) {
+    do 
+      ++i; 
+    while (elements[i] <= pivot && i <= r);
+
+    do 
+      --j; 
+    while (elements[j] > pivot);
+
+    if( i >= j ) break;
+
+    t = elements[i]; 
+    elements[i] = elements[j]; 
+    elements[j] = t;
+  }
+  
+  t = elements[l]; 
+  elements[l] = elements[j]; 
+  elements[j] = t;
+  
+  return j;
+}
+
+void main()  {
+  int seed, rank, nthreads, t, i, j, rc;
+  struct thread_data index_left;
+  void *status;
+  int pivot;
+  int ttime;
+
+  ttime = timer();
+  seed = time(NULL);
+  srand(seed);
+
+  pthread_t thread;
+  pthread_attr_t attr;
+
+  pthread_attr_init(&attr);
+  pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+  pthread_mutex_init(&mutexsort, NULL);
+
+  elements = (int *)malloc(INTERVALS*sizeof(int));  
+
+  for(j = 0; j <= INTERVALS; j++) {
+    elements[j] = (rand() % 999 +1);
+  }
+
+  if (DEBUG) {
+    printf("\n\nUnsorted array is:  ");
+    for(i = 0; i < INTERVALS; ++i) {
+      printf(" %d ", elements[i]);
+    }
+    printf("\n");
+  }
+  
+  index_left.start_index = 0;
+  index_left.end_index =  INTERVALS-1;
+
+  rc = pthread_create(&thread, &attr, qsort_parallel, (void *) &index_left);
+
+  pthread_join(thread, &status);
+
+  if (DEBUG) {
+    printf("\n\nSorted array is:  ");
+    for(i = 0; i < INTERVALS; ++i) {
+      printf(" %d ", elements[i]);
+    }
+    printf("\n");
+  }
+
+  ttime = timer()-ttime;
+  printf("\nTime: %f, Threads: %d\n\n", ttime/1000000.0, thread_count);
+
+  pthread_mutex_destroy(&mutexsort);
+  pthread_attr_destroy(&attr);
+  free(elements);
+  
+  pthread_exit(NULL);
+}
+
